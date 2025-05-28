95278acb317df972ce9511da6760165a266baf65
itachi9018 <itachi9018@gmail.com>
2025-05-29 02:04:10 +0530

    Adding config manually for gh-pages

--- deploy.js
+++ deploy.js
@@ -1,9 +1,9 @@
 
-const ghpages = require('gh-pages');
+import ghpages from 'gh-pages';
 
 ghpages.publish('dist', {
   branch: 'gh-pages',
-  repo: 'https://github.com/yourusername/emotiq-mood-flow.git', // Update this with your repo URL
+  repo: 'https://github.com/itachi9018/emotiq-mood-flow.git', // Update this with your repo URL
   user: {
     name: 'GitHub Actions',
     email: 'actions@github.com'
